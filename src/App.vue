0e272f3bcc724798178d085958dafc21d25636c7
zll <1161555027@qq.com>
2022-09-12 19:50:33 +0800

    首页 底部导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="dockor">
+    <div class="dockor__item dockor__item--active">
+      <div class="iconfont">&#xe69b;</div>
+      <div class="dockor__title">首页</div>
+    </div>
+    <div class="dockor__item">
+      <div class="iconfont">&#xe6e6;</div>
+      <div class="dockor__title">购物车</div>
+    </div>
+    <div class="dockor__item">
+      <div class="iconfont">&#xe671;</div>
+      <div class="dockor__title">订单</div>
+    </div>
+    <div class="dockor__item">
+      <div class="iconfont">&#xe62f;</div>
+      <div class="dockor__title">我的</div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.dockor {
+  display: flex;
+  position: absolute;
+  box-sizing: border-box;
+  padding: 0 0.18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #f1f1f1;
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
     }
+    &--active {
+      color: #1fa4fc;
+    }
+  }
+  &__title {
+    font-size: 20px;
+    transform: scale(0.5, 0.5);
+    transform-origin: center top;
   }
 }
 </style>
